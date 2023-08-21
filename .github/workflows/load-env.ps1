ef8c5831cdc3caf0b5772125b6bea8a851dbbf7f
ajaykumarnanda07 <137915419+ajaykumarnanda07@users.noreply.github.com>
2023-08-21 13:53:15 +0530

    Create load-env.ps1

---
+++ .github/workflows/load-env.ps1
@@ -0,0 +1,8 @@
+# Read the contents of the .env file
+$envContents = Get-Content -Path ".env"
+
+# Parse the contents and set environment variables
+foreach ($line in $envContents) {
+    $key, $value = $line -split "=", 2
+    $env:$key = $value
+}
